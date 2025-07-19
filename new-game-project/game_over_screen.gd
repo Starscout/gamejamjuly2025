ab08ebce37bede2879ffa027d863a42a2b0e3a09
bacchylides <83319694+bacchylides@users.noreply.github.com>
2025-07-19 15:21:47 -0700

    a.

--- new-game-project/game_over_screen.gd
+++ new-game-project/game_over_screen.gd
@@ -8,6 +8,7 @@ func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
 	
 
+#TODO: Need to stop the player from moving when the timer runs out.
 func _on_timer_ui_node_time_is_out():
 	$GameOverPanel.visible = true
 	$GameOverLabel.visible = true
