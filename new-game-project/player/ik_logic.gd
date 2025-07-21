96713a51dabed180b14df1e44fe07995315cbc82
Starscout <Polleysjonathan@gmail.com>
2025-07-20 23:08:32 -0600

    Right hand goes up and somewhat down

--- new-game-project/player/ik_logic.gd
+++ new-game-project/player/ik_logic.gd
@@ -1,26 +1,37 @@
 extends Node2D
 
 var parent 
-var right_hand
+var right_hand_target_position
 var left_hand
 
 var max_frame = 30
 var frame = 0
 
 func _ready():
-	parent = get_parent()
-	right_hand = $"IK Targets/Right_Hand_target"
-	left_hand = $"IK Targets/Left_hand_target"
-
+	parent = get_parent() 
+	right_hand_target_position = Vector2(-300, -8)
+	set_target_point()
 func _process(delta):
-	if frame < max_frame:
-		frame += 1
-	else:
-		frame = 0
+	
+	
+	if parent.global_position.y < right_hand_target_position.y:
 		set_target_point()
 	
-func get_direction():
-	pass
+	#$"IK Targets/Right_Hand_target".position.y = clamp($"IK Targets/Right_Hand_target".position.y, -150, 999)
 
+	if $"IK Targets/Right_Hand_target".position.y < -150:
+		right_hand_target_position.y += 5
+	$"IK Targets/Right_Hand_target".global_position = right_hand_target_position
+
+
+
+func get_direction():
+	return (parent.velocity)
+	
 func set_target_point():
-	$"IK Targets/Right_Hand_target".position.y = randi_range(-100, 100)
+	if $"IK Targets/Right_Hand_target".position.y > -150:
+		var global_posit = $"IK Targets/Right_Hand_target".get_global_position()
+		var tween = create_tween()
+		tween.tween_method(func(value): right_hand_target_position.y = value, global_posit.y, global_posit.y-100,.2)
+		
+		right_hand_target_position = global_posit
