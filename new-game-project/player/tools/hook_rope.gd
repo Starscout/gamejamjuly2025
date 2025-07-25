3edbf0af4ee8472f0f45f57ddce80b9037617c85
bacchylides <83319694+bacchylides@users.noreply.github.com>
2025-07-25 14:47:37 -0700

    Added comments to the 'hook_rope' script.
    Added comments to the 'hook_rope' script to help following along.

--- new-game-project/player/tools/hook_rope.gd
+++ new-game-project/player/tools/hook_rope.gd
@@ -21,6 +21,7 @@ func _physics_process(delta: float) -> void:
 		motion *= 0.975 # Speed of swing
 		
 		# Here's where we'd affect player velocity based on motion calculations
+		#You slowfall because 'motion.y' == 0, then set that equal to the 'velocity.y'. Why setting the velocity to 0 doesn't stop us, I don't know
 		player.velocity.x = motion.x
 		player.velocity.y = motion.y
 	
@@ -37,7 +38,7 @@ func _draw():
 		if colliding and position.distance_to(collide_point) < length:
 			draw_line(Vector2(0,0), to_local(collide_point), Color(1,1,1,0.25), 0.5, true)
 			 
-
+#Simply has the raycasts look at our mouse and when we press the button we set the hook_position to where the collision happened and then get the length to there
 func hook() -> void:
 	$Raycasts.look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("activate_tool"):
@@ -48,6 +49,7 @@ func hook() -> void:
 	if Input.is_action_just_released("activate_tool") and hooked:
 		hooked = false
 
+#This gets what raycast collided what where we colliding.
 func get_hook_position():
 	for raycast in $Raycasts.get_children():
 		if raycast.is_colliding():
@@ -55,12 +57,15 @@ func get_hook_position():
 
 func swing(delta):
 	var radius = global_position - hook_position
-	if motion.length() < 0.01 or radius.length() > 10: return
+	#motion.length() is always 0 right now and radius.length() is always greater than 10, so we don't go forward from here.
+	if motion.length() < 0.01 or radius.length() > 10: return 
+	#If we do, however, we will crash because of dividing by zero, as motion.length() alwasy seems to be 0.
 	var angle = acos(radius.dot(motion) / (radius.length() * motion.length()))
 	var rad_vel = cos(angle) * motion.length()
+	#I assume, though I do not understand the 'acos' and 'cos' stuff, that this is trying to make the player move in angle of the collision.
 	motion += radius.normalized() * -rad_vel
 	
 	if global_position.distance_to(hook_position) > length:
 		global_position = hook_position + radius.normalized() * length
-	
+	#This also crashes the game, for some reason?
 	motion += (hook_position - global_position.normalized) * 15000 * delta
