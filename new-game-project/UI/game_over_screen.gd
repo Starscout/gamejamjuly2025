8ad67a1458b46b3a9d1c91e4874ce71f5e46efa6
bacchylides <83319694+bacchylides@users.noreply.github.com>
2025-07-28 19:29:37 -0700

    New game over menu.
    As described on the tin.

--- new-game-project/UI/game_over_screen.gd
+++ new-game-project/UI/game_over_screen.gd
@@ -2,7 +2,6 @@ extends Control
 
 func _ready() -> void:
 	$GameOverPanel.visible = false
-	$GameOverLabel.visible = false
 
 func _on_restart_button_pressed():
 	get_tree().reload_current_scene()
@@ -10,4 +9,3 @@ func _on_restart_button_pressed():
 #TODO: Need to stop the player from moving when the timer runs out.
 func _on_timer_ui_node_time_is_out():
 	$GameOverPanel.visible = true
-	$GameOverLabel.visible = true
